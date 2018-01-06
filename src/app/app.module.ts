e358eecdbe595d465436b43e085d3170a7591f74
Alexander Katulskiy <alexxty7@gmail.com>
2018-01-06 15:41:27 +0200

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import {
+  MatToolbarModule,
+  MatCardModule
+} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
+import { ImgCardComponent } from './img-card/img-card.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImgCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
